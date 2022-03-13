e90d64c4ede79775519e09989be9446a334c6995
nio0o <joaovictoria456@gmail.com>
2022-03-13 20:37:50 -0300

    ok

--- app.py
+++ app.py
@@ -1,16 +1,48 @@
-nota1 = float(input("Digite a primeira nota a ser inserida?"))
-nota3 = float(input("Digite a terceira nota a ser inserida?"))
-nota2 = float(input("Digite a segunda nota a ser inserida?"))
 
-media = nota1 + nota2 + nota3 / 3
+#nota1 = (input("Digite a primeira nota a ser inserida?"))
+#nota3 = (input("Digite a terceira nota a ser inserida?"))
+#nota2 = (input("Digite a segunda nota a ser inserida?"))
 
-if (media >=7):
-    print("APROVADO")
+#media = nota1 + nota2 + nota3 / 3
 
-elif(media >= 5) and (media< 7):
-    print("RECUPERAÇÃO")
+#if (media >=7):
+#    print("APROVADO")
+#
+#elif(media >= 5) and (media< 7):
+#    print("RECUPERAÇÃO")
+#
+#else:
+#    print("REPROVADO")
+#
 
-else:
-    print("REPROVADO")
+valor = 50
+ 
+print(valor)
 
-    
+if valor <= 1830.29:
+    valor -= valor * 0.08
+    print("desconto aplicado de 0.08")
+    print()
+elif valor <= 3050.52:
+    print("desconto aplicado de 0.09")
+    valor -= valor * 0.09
+elif valor <= 6101.06:
+    print("desconto aplicado de 0.11")
+    valor -= valor * 0.11
+
+
+
+def calcular_desconto(valor):
+ 
+if valor <= 100:
+    valor -= valor * 0.03
+
+elif valor <= 500:
+    valor -= valor * 0.10
+
+elif valor <= 1000:
+    valor -= valor * 0.20
+
+return valor
+
+
